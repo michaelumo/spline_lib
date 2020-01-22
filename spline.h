2e0938a04ca1d85ba777e888c0ad7d64816c350d
michaelumo <spikeumo@gmail.com>
2020-01-22 11:27:38 +0900

    fixed some bug

--- spline.h
+++ spline.h
@@ -16,6 +16,7 @@ class Spline{
     std::vector<float> accel;
     Spline();
     void cspline(Matrix &in);
+    void cspline2(Matrix &in);
     void bspline(Matrix &in, int k);
     void calc_point(double i, Matrix &in);
     void calc_vec(double u, Matrix &in, Matrix &vec);
@@ -58,18 +59,53 @@ void Spline::cspline(Matrix &in){//type of matrix is (t,x,y,z..).t()
   }
 }
 
+void Spline::cspline2(Matrix &in){//type of matrix is (t,x,y,z..).t()
+  f.clear();
+  int num = in.getRows();
+  TDMA alg;
+  point = std::vector<float>(num);
+  velocity = std::vector<float>(num);
+  accel = std::vector<float>(num);
+  for(int n = 1; n < num; n++){
+  	std::vector<double> A;
+    std::vector<double> B;
+    std::vector<double> C;
+    std::vector<double> D;
+    h.clear();
+    h.push_back(0);
+    //calculate delta t
+    for(int i = 1; i < in.getCols(); i++){
+      h.push_back(in(0,i)-in(0,i-1));
+    }
+    A.push_back(0);//h[0]/(h[0]+h[1])); dont care
+    B.push_back(2.0);//2.0*(h[-1]+h[0])
+    C.push_back(1.0);//h[1]/h[1]
+    D.push_back(6.0*((in(n,1)-in(n,0))/h[1])/h[1]-6.0*0);
+    for(int i = 1; i < h.size(); i++){
+      B.push_back(2.0);
+      if(i != h.size()-1)C.push_back(h[i+1]/(h[i]+h[i+1]));
+      if(i != h.size()-1)A.push_back(h[i]/(h[i]+h[i+1]));
+      if(i != h.size()-1)D.push_back(6.0*((in(n,i+1)-in(n,i))/h[i+1]-((in(n,i)-in(n,i-1))/h[i]))/(in(0,i+1)-in(0,i-1)));
+    }
+    A.push_back(1.0);
+    C.push_back(0);//dont care
+    D.push_back(-6.0*((in(n, h.size()-1)-in(n, h.size()-2))/h[h.size()-1])/h[h.size()-1]);
+    f.push_back(alg.tdma(A,B,C,D));
+  }
+}
+
 void Spline::calc_point(double i, Matrix &in){
   int j = 0;
-  for(int k = 0; k < in.getCols()-1; k++){
-    if(in(0,k) <= i && i<=in(0,k+1)){
+  for(int k = 1; k < in.getCols(); k++){
+    if(in(0,k-1) <= i && i<=in(0,k)){
       j = k;
       break;
     }
   }
   for(int n = 0; n < in.getRows()-1; n++){
-    point.insert(point.begin()+n, f[n](j+1)/(6.0*h[j])*pow((i-in(0,j)),3.0)+f[n](j)/(6.0*h[j])*pow((in(0,j+1)-i),3.0)+(in(n+1,j+1)/h[j]-f[n](j+1)/6.0*h[j])*(i-in(0,j))+(in(n+1,j)/h[j]-f[n](j)/6.0*h[j])*(in(0,j+1)-i));
-    velocity.insert(velocity.begin()+n, f[n](j+1)/(2.0*h[j])*pow((i-in(0,j)),2.0)-f[n](j)/(2.0*h[j])*pow((in(0,j+1)-i),2.0)+(in(n+1,j+1)/h[j]-f[n](j+1)/6.0*h[j])-(in(n+1,j)/h[j]-f[n](j)/6.0*h[j]));
-    accel.insert(accel.begin()+n, f[n](j)/h[j]*(in(0,j+1)-i) + f[n](j+1)/h[j]*(i-in(0,j)));
+    point.insert(point.begin()+n, f[n](j)/(6.0*h[j])*pow((i-in(0,j-1)),3.0)+f[n](j-1)/(6.0*h[j])*pow((in(0,j)-i),3.0)+(in(n+1,j)/h[j]-f[n](j)/6.0*h[j])*(i-in(0,j-1))+(in(n+1,j-1)/h[j]-f[n](j-1)/6.0*h[j])*(in(0,j)-i));
+    velocity.insert(velocity.begin()+n, f[n](j)/(2.0*h[j])*pow((i-in(0,j-1)),2.0)-f[n](j-1)/(2.0*h[j])*pow((in(0,j)-i),2.0)+(in(n+1,j)/h[j]-f[n](j)/6.0*h[j])-(in(n+1,j-1)/h[j]-f[n](j-1)/6.0*h[j]));
+    accel.insert(accel.begin()+n, f[n](j-1)/h[j]*(in(0,j)-i) + f[n](j)/h[j]*(i-in(0,j-1)));
   }
 }
 
